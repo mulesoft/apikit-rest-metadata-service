75c6e099e4ec9162ebab7fd365d2e2ec15d415c8
Karan Shah <karan.shah@salesforce.com>
2025-01-08 01:17:54 -0800

    W-17537708 : Increase Test coverage

--- src/test/java/org/mule/module/apikit/metadata/internal/model/ApikitElementIdentifiersTest.java
+++ src/test/java/org/mule/module/apikit/metadata/internal/model/ApikitElementIdentifiersTest.java
@@ -1,4 +1,5 @@
 package org.mule.module.apikit.metadata.internal.model;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mule.runtime.api.component.ComponentIdentifier;
@@ -8,63 +9,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ApikitElementIdentifiersTest {
 
-    @Test
-    void testIsFlow() {
-        ComponentAst component = Mockito.mock(ComponentAst.class);
-        ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("flow");
-        Mockito.when(component.getIdentifier()).thenReturn(identifier);
+  @Test
+  void testIsFlow() {
+    ComponentAst component = Mockito.mock(ComponentAst.class);
+    ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("flow");
+    Mockito.when(component.getIdentifier()).thenReturn(identifier);
 
-        assertTrue(ApikitElementIdentifiers.isFlow(component));
-    }
+    assertTrue(ApikitElementIdentifiers.isFlow(component));
+  }
 
-    @Test
-    void testIsNotFlow() {
-        ComponentAst component = Mockito.mock(ComponentAst.class);
-        ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("not-flow");
-        Mockito.when(component.getIdentifier()).thenReturn(identifier);
+  @Test
+  void testIsNotFlow() {
+    ComponentAst component = Mockito.mock(ComponentAst.class);
+    ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("not-flow");
+    Mockito.when(component.getIdentifier()).thenReturn(identifier);
 
-        assertFalse(ApikitElementIdentifiers.isFlow(component));
-    }
+    assertFalse(ApikitElementIdentifiers.isFlow(component));
+  }
 
-    @Test
-    void testIsApikitConfig() {
-        ComponentAst component = Mockito.mock(ComponentAst.class);
-        ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("apikit:config");
-        Mockito.when(component.getIdentifier()).thenReturn(identifier);
+  @Test
+  void testIsApikitConfig() {
+    ComponentAst component = Mockito.mock(ComponentAst.class);
+    ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("apikit:config");
+    Mockito.when(component.getIdentifier()).thenReturn(identifier);
 
-        assertTrue(ApikitElementIdentifiers.isApikitConfig(component));
-    }
+    assertTrue(ApikitElementIdentifiers.isApikitConfig(component));
+  }
 
-    @Test
-    void testIsNotApikitConfig() {
-        ComponentAst component = Mockito.mock(ComponentAst.class);
-        ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("not-apikit:config");
-        Mockito.when(component.getIdentifier()).thenReturn(identifier);
+  @Test
+  void testIsNotApikitConfig() {
+    ComponentAst component = Mockito.mock(ComponentAst.class);
+    ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("not-apikit:config");
+    Mockito.when(component.getIdentifier()).thenReturn(identifier);
 
-        assertFalse(ApikitElementIdentifiers.isApikitConfig(component));
-    }
+    assertFalse(ApikitElementIdentifiers.isApikitConfig(component));
+  }
 
-    @Test
-    void testIsFlowMappings() {
-        ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("apikit:flow-mappings");
-        assertTrue(ApikitElementIdentifiers.isFlowMappings(identifier));
-    }
+  @Test
+  void testIsFlowMappings() {
+    ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("apikit:flow-mappings");
+    assertTrue(ApikitElementIdentifiers.isFlowMappings(identifier));
+  }
 
-    @Test
-    void testIsNotFlowMappings() {
-        ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("not-apikit:flow-mappings");
-        assertFalse(ApikitElementIdentifiers.isFlowMappings(identifier));
-    }
+  @Test
+  void testIsNotFlowMappings() {
+    ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("not-apikit:flow-mappings");
+    assertFalse(ApikitElementIdentifiers.isFlowMappings(identifier));
+  }
 
-    @Test
-    void testIsFlowMapping() {
-        ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("apikit:flow-mapping");
-        assertTrue(ApikitElementIdentifiers.isFlowMapping(identifier));
-    }
+  @Test
+  void testIsFlowMapping() {
+    ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("apikit:flow-mapping");
+    assertTrue(ApikitElementIdentifiers.isFlowMapping(identifier));
+  }
 
-    @Test
-    void testIsNotFlowMapping() {
-        ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("not-apikit:flow-mapping");
-        assertFalse(ApikitElementIdentifiers.isFlowMapping(identifier));
-    }
+  @Test
+  void testIsNotFlowMapping() {
+    ComponentIdentifier identifier = ComponentIdentifier.buildFromStringRepresentation("not-apikit:flow-mapping");
+    assertFalse(ApikitElementIdentifiers.isFlowMapping(identifier));
+  }
 }
